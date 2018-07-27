4a708dda6f0a0f5b32bfd1209f8ac95d37f2c602
lwh_glb <2292033520@qq.com>
2018-07-27 11:03:55 +0800

    edit by oxygen 2

--- src/main/java/com/lwh/git/Apple.java
+++ src/main/java/com/lwh/git/Apple.java
@@ -2,7 +2,7 @@ package com.lwh.git;
 
 public class Apple {
     public static void main(String[] args) {
-        System.out.println("ok!");
+        System.out.println("ok! TestGit2");
     }
 
 }
