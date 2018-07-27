90f8d50d24e19ebf82bbe64a7e368f25f324f8f0
lwh_glb <2292033520@qq.com>
2018-07-27 11:49:01 +0800

    Merge branch 'hot_fix' BETTER

--- src/main/java/com/lwh/git/Apple.java
+++ src/main/java/com/lwh/git/Apple.java
@@ -2,15 +2,14 @@ package com.lwh.git;
 
 public class Apple {
     public static void main(String[] args) {
-<<<<<<< HEAD
 
         System.out.println("ok! TestGit2");
 
         System.out.println("ok! TestGit");
 
-=======
+
         System.out.println("HOt FIX");
->>>>>>> refs/heads/hot_fix
+
     }
 
 }
