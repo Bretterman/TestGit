4a34814023dec42fc7a2f9b2854de4a3a930f6ca
lwh_glb <2292033520@qq.com>
2018-07-27 11:38:21 +0800

    hOT FIX

--- src/main/java/com/lwh/git/Apple.java
+++ src/main/java/com/lwh/git/Apple.java
@@ -2,7 +2,7 @@ package com.lwh.git;
 
 public class Apple {
     public static void main(String[] args) {
-        System.out.println("ok! TestGit");
+        System.out.println("HOt FIX");
     }
 
 }
